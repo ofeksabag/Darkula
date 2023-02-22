26fac9a6f95a6149b1a91b4215055be7aac575e9
ofeksabag <osabag55@gmail.com>
2023-02-22 22:03:41 +0200

    .

--- src/content/content.ts
+++ src/content/content.ts
@@ -2,5 +2,5 @@ const all = document.querySelectorAll("*");
 
 for(const element of all) {
     (element as HTMLElement).style.backgroundColor = "#111111";
-    (element as HTMLElement).style.color = "White";
+    (element as HTMLElement).style.color = "#FFF";
 }
